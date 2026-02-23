d51adb435edc4006981842970059a3bb0ec5c9c0
jlriom <jlriom@hotmail.es>
2026-02-23 13:22:28 +0100

    end section 3

--- MyOverflow.AppHost/AppHost.cs
+++ MyOverflow.AppHost/AppHost.cs
@@ -1,6 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+#pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithoutHttpsCertificate()
+#pragma warning restore ASPIRECERTIFICATES001
     .WithDataVolume("keycloak-data");
+
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb  =postgres.AddDatabase("questionDb");
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
 
 builder.Build().Run();
---
+++ Services/QuestionService/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+builder.Services.AddAuthentication()
+    .AddKeycloakJwtBearer(serviceName: "keycloak", realm: "overflow", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "overflow";
+    });
+
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapDefaultEndpoints();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while migrating the database.");
+}
+
+app.Run();
